9cfcd7f96414c1c4132ddfe570d34a6f2ef354b3
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-15 00:17:07 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,112 @@
+//if1
+//a < b
+// let a = 2;
+// let b = 7;
+
+// for (i = a; i <= b; i++) {
+//     console.log(i);
+// }
+
+//if2
+
+// let a = 2;
+// let b = 7;
+
+// for (i = b; i >= a; i--) {
+//     console.log(i);
+// }
+
+//if3
+
+// let kg = 1000;
+
+// for (i = 1; i <= 10; i ++) {
+//     console.log(kg * i);
+// }
+
+//if4
+
+// let kg = 1000;
+
+// for (i = 12; i <= 20; i ++) {
+//     console.log((kg * i) / 10);   
+// }
+
+
+//if5
+
+// let sum = 0;
+// let a = 3;
+// let b = 8;
+
+// for(i = a; i <= b; i++){
+//     sum += i
+// }
+
+// console.log(sum);
+
+//if6
+// let mul = 1;
+// let a = 3;
+// let b = 8;
+
+// for(i = a; i <= b; i++){
+//     mul *= i
+// }
+
+// console.log(mul);
+
+
+//if7
+
+// let sum = 0;
+// let a = 3;
+// let b = 8;
+
+// for(i = a; i <= b; i++) {
+//     sum = sum + i ** 2
+// }
+
+// console.log(sum);
+
+//if8
+
+// let n = 2;
+// let S = 0;
+
+// for (i = 1; i <= n; i++) {
+//     console.log((S = 1/ i )) / 20;
+// }
+
+//if9
+
+
+// let n = 2;
+// let S = 1;
+
+// for (i = 1.1; i <= n; i += 0.1) {
+//     (S  = S * i);
+// }
+
+// console.log(S);
+
+
+//if11
+
+// let n = 6;
+// let s = 2;
+
+// for (let i = 1; i <= n; i++) {
+//     console.log(i, Math.pow(s, i));
+// }
+
+//if12
+
+// let n = 5;
+// let a = 10;
+// let mul = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   mul *= a;
+// }
+// console.log(mul);
